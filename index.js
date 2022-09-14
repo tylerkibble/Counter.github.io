e90530b6d7fca1b389b7ad3eaa946f86e9a49049
Tyler Kibble <44215929+tylerkibble@users.noreply.github.com>
2022-09-14 15:20:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
+
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0
+
+
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+let dash = " - "
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    saveEl.innerText += " " + count + dash
+    console.log("this is the updated count")
+    console.log(count)
+    count = 0
+}
+
+
+
 
